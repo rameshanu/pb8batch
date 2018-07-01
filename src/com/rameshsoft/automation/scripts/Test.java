f157c2bda2ab1dad013bff7df02445a0920b2eb9
rameshanu <javaramesh456@gmail.com>
2018-07-01 11:04:10 +0530

    java 12233

--- src/com/rameshsoft/automation/scripts/Test.java
+++ src/com/rameshsoft/automation/scripts/Test.java
@@ -6,6 +6,7 @@ public static void main(String[] args) {
 	System.out.println("hello");
 	System.out.println("helo 12345");
 	System.out.println("hello how are you....");
+	System.out.println("789");
 
 	}
 }
