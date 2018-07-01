80177d60599b1555ff8fda75ceedfd88281c74a4
rameshanu <javaramesh456@gmail.com>
2018-07-01 11:13:33 +0530

    java 123456

--- src/com/rameshsoft/automation/scripts/ATest.java
+++ src/com/rameshsoft/automation/scripts/ATest.java
@@ -4,5 +4,6 @@ public class ATest {
 public static void main(String[] args) {
 	System.out.println("Hello Dont sleep");
 System.out.println("added");	
+System.out.println("Monitor the things...");
 }
 }
--- src/com/rameshsoft/automation/scripts/Test.java
+++ src/com/rameshsoft/automation/scripts/Test.java
@@ -7,6 +7,6 @@ public static void main(String[] args) {
 	System.out.println("helo 12345");
 	System.out.println("hello how are you....");
 	System.out.println("789");
-
+System.out.println("123");
 	}
 }
