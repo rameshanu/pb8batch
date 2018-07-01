c2ef1020fe446117dac6efad9619cf5b754bf5d6
rameshanu <javaramesh456@gmail.com>
2018-07-01 08:50:36 +0530

    first commit by Ramesh

---
+++ src/com/rameshsoft/automation/scripts/ATest.java
@@ -0,0 +1,8 @@
+package com.rameshsoft.automation.scripts;
+
+public class ATest {
+public static void main(String[] args) {
+	
+	
+}
+}
---
+++ src/com/rameshsoft/automation/scripts/Test.java
@@ -0,0 +1,8 @@
+package com.rameshsoft.automation.scripts;
+
+public class Test {
+public static void main(String[] args) {
+	
+	System.out.println("hello");
+}
+}
